20daf82719aa2e9106811368c1c0efe3448bfeee
KennyS404 <kennyldo48@gmail.com>
2023-05-16 21:37:27 -0300

    Name corrected

--- src/test/java/com/kennildo/kennildo/KennildoApplicationTests.java
+++ src/test/java/com/kennildo/kennildo/KennildoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DesbravadorApplicationTests {
+class KennildoApplicationTests {
 
 	@Test
 	void contextLoads() {
